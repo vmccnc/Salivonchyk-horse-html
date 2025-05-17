e8349c0fa99a269948e8babb2a32a73797a206de
Dmitry Prokopenko <6558822@gmail.com>
2025-05-17 16:29:41 +0200

    [Fix] - index.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 // scripts.js
 
+
+// header.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the current page's filename (if empty, default to "index.html")
+    let currentPage = window.location.pathname.split('/').pop();
+    if (!currentPage) {
+      currentPage = "index.html";
+    }
+    // Add the 'active' class to the link that matches the current page
+    const links = document.querySelectorAll('.header-container nav ul li a');
+    links.forEach(link => {
+      const href = link.getAttribute('href');
+      if (href === currentPage) {
+        link.classList.add('active');
+      }
+    });
+  });
+  
+
 document.addEventListener('DOMContentLoaded', () => {
   // HORSES PAGE FILTER & SORT
   const horseCards = document.querySelectorAll('.horse-card');
